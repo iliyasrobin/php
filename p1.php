102260d28ca6b997bc8f18679f33032fe9d63cba
Iliyas Robin <iliyasahmedr@gmail.com>
2025-06-28 20:36:16 +0600

    class 1 reviewing

--- p1.php
+++ p1.php
@@ -1,5 +1,23 @@
 <?php
 
-echo "<h1>Hello World!</h1>";
+echo "Hello World!";
+
+echo "\n";
+$name = 'iliyas';
+$name2 = 'ahmed';
+
+echo $name;
+echo PHP_EOL;
+echo $name2;
+echo PHP_EOL;
+echo $name2;
+
+echo PHP_EOL;
+
+//taking input 
+
+$name3 = readline('ENTER YOUR NAME');
+echo PHP_EOL;
+echo $name3;
 
 ?>
